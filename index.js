c0ee53576eb68b7f9c14ddec7085960e038460e3
Nandani <nks854338@gmail.com>
2024-04-28 16:14:10 +0530

    Files are inserted

---
+++ index.js
@@ -0,0 +1,191 @@
+let audio = document.querySelector("#audio");
+let ShowSong = document.getElementById("ShowSong");
+let menu = document.getElementById("menu");
+let menus = document.getElementById("menus");
+let musicapps = document.getElementById("musicapps");
+let musicapp = document.getElementById("musicapp");
+let play = document.getElementById("play");
+let backward = document.getElementById("backward");
+let forward = document.getElementById("forward");
+let songName = document.getElementById("songName");
+let singerName = document.getElementById("singerName");
+let song1 = document.getElementById("song1");
+let song2 = document.getElementById("song2");
+let song3 = document.getElementById("song3");
+let song4 = document.getElementById("song4");
+let song5 = document.getElementById("song5");
+
+let pauseIcon = '<i class="fa-solid fa-circle-play" style="color: #566573"></i>';
+let playIcon = '<i class="fa-solid fa-pause" style="color: #566573;"></i>';
+let currentSongIndex = 0;
+
+let songCurrTime = "";
+
+
+let songlist = [
+    ["songs/lagja_gale_ki_phir_ye.mp3", "Lagja gale ki phir ye", "Lata Mangeshkar"],
+    ["songs/lo_safar_shuru_ho_gaya.mp3", "Lo safar shuru ho gaya", "Jubin Nautiyal"],
+    ["songs/mere_mehboob_qayamat_hogi.mp3", "Mere mehboob qayamat hogi", "Kishore kumar"],
+    ["songs/pyar_deewana_hota_hai_mastana_hota_hai.mp3", "Pyar deewana hota hai", "Kishore kumar"],
+    ["songs/ye_raate_ye_mausam.mp3", "Ye raate ye mausam", "Sanam, Simran Sehgal"],
+];
+
+
+
+function defaultSong() {
+    audio.src = songlist[currentSongIndex][0];
+    songName.innerText = songlist[currentSongIndex][1];
+    singerName.innerText = songlist[currentSongIndex][2];
+    play.innerHTML = pauseIcon;
+    playSong()
+
+}
+
+
+
+
+
+song1.addEventListener('click', () => {
+    currentSongIndex = 0;
+    musicapp.classList.add('hide');
+    musicapps.classList.remove('hide');
+    play.innerHTML = playIcon;
+    playSong()
+})
+
+song2.addEventListener('click', () => {
+    currentSongIndex = 1;
+    musicapp.classList.add('hide');
+    musicapps.classList.remove('hide');
+    play.innerHTML = playIcon;
+    playSong()
+})
+
+song3.addEventListener('click', () => {
+    currentSongIndex = 2;
+    musicapp.classList.add('hide');
+    musicapps.classList.remove('hide');
+    play.innerHTML = playIcon;
+    playSong();
+})
+
+song4.addEventListener('click', () => {
+    currentSongIndex = 3;
+    musicapp.classList.add('hide');
+    musicapps.classList.remove('hide');
+    play.innerHTML = playIcon;
+    playSong();
+})
+
+song5.addEventListener('click', () => {
+    currentSongIndex = 4;
+    musicapp.classList.add('hide');
+    musicapps.classList.remove('hide');
+    play.innerHTML = playIcon;
+    playSong();
+})
+
+
+
+
+
+function pauseSong() {
+    audio.pause();
+    songCurrTime = audio.currentTime;
+}
+
+
+function playSong() {
+    audio.src = songlist[currentSongIndex][0];
+    songName.innerText = songlist[currentSongIndex][1];
+    singerName.innerText = songlist[currentSongIndex][2];
+    audio.currentTime = songCurrTime;
+    audio.play();
+}
+
+function nextSong() {
+    currentSongIndex++;
+    ShowSong.value == audio.duration;
+    if (currentSongIndex >= songlist.length) {
+        currentSongIndex = 0;
+    }
+    playSong();
+}
+
+ShowSong.length == audio.duration;
+
+audio.onended = nextSong;
+
+
+
+if (audio.play()) {
+    setInterval(() => {
+        ShowSong.value = audio.currentTime;
+        if (ShowSong.value == audio.duration) {
+            ShowSong.value = "0";
+            nextSong();
+        }
+    }, 500);
+}
+
+
+play.addEventListener('click', () => {
+    if (play.innerHTML === pauseIcon) {
+        play.innerHTML = playIcon;
+        ShowSong.value = audio.currentTime;
+        audio.play();
+        changeSongTiming();
+    }
+    else {
+        play.innerHTML = pauseIcon;
+        audio.pause();
+        changeSongTiming();
+
+    }
+})
+
+
+
+
+ShowSong.addEventListener('click', () => {
+    audio.currentTime = ShowSong.value;
+});
+
+
+
+
+function changeSongTiming() {
+    forward.addEventListener('click', () => {
+        audio.currentTime += 5;
+    })
+
+    backward.addEventListener('click', () => {
+        audio.currentTime -= 5;
+    })
+}
+
+
+
+
+function pageSwap() {
+    menus.addEventListener('click', () => {
+        musicapps.classList.add('hide');
+        musicapp.classList.remove('hide');
+    })
+
+    menu.addEventListener('click', () => {
+        musicapp.classList.add('hide');
+        musicapps.classList.remove('hide');
+    })
+}
+
+audio.onloadedmetadata = function () {
+    ShowSong.max = audio.duration;
+};
+
+
+
+
+defaultSong();
+changeSongTiming();
+pageSwap();
