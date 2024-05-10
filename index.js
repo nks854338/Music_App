89e7fd3982d188b4a403bb102d8095883d4ff5c9
Nandani <nks854338@gmail.com>
2024-05-11 02:07:59 +0530

    New songs are added

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ let song2 = document.getElementById("song2");
 let song3 = document.getElementById("song3");
 let song4 = document.getElementById("song4");
 let song5 = document.getElementById("song5");
+let song6 = document.getElementById("song6");
+let song7 = document.getElementById("song7");
 
 let pauseIcon = '<i class="fa-solid fa-circle-play" style="color: #566573"></i>';
 let playIcon = '<i class="fa-solid fa-pause" style="color: #566573;"></i>';
@@ -28,6 +30,8 @@ let songlist = [
     ["songs/mere_mehboob_qayamat_hogi.mp3", "Mere mehboob qayamat hogi", "Kishore kumar"],
     ["songs/pyar_deewana_hota_hai_mastana_hota_hai.mp3", "Pyar deewana hota hai", "Kishore kumar"],
     ["songs/ye_raate_ye_mausam.mp3", "Ye raate ye mausam", "Sanam, Simran Sehgal"],
+    ["songs/Ek_Ajnabee_Haseena_Se.mp3", "Ek Ajnabee Haseena Se", "Sanam, Simran Sehgal"],
+    ["songs/Kya_Kua_Tera_Wada.mp3", "Kya Kua Tera Wada", "Sanam, Simran Sehgal"]
 ];
 
 
@@ -79,6 +83,22 @@ song4.addEventListener('click', () => {
 
 song5.addEventListener('click', () => {
     currentSongIndex = 4;
+    musicapp.classList.add('hide');
+    musicapps.classList.remove('hide');
+    play.innerHTML = playIcon;
+    playSong();
+})
+
+song6.addEventListener('click', () => {
+    currentSongIndex = 5;
+    musicapp.classList.add('hide');
+    musicapps.classList.remove('hide');
+    play.innerHTML = playIcon;
+    playSong();
+})
+
+song7.addEventListener('click', () => {
+    currentSongIndex = 6;
     musicapp.classList.add('hide');
     musicapps.classList.remove('hide');
     play.innerHTML = playIcon;
